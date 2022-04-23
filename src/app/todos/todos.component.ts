0a195358ac74ecf8756b5a57b52e3379d82c07cd
Adrian Juszczak <adrian@juszczak.io>
2022-04-23 11:46:34 +0200

    Todo Interface
    Signed-off-by: Adrian Juszczak <adrian@juszczak.io>

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Todo} from './model/todo';
 
 @Component({
   selector: 'app-todos',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todos.component.scss']
 })
 export class TodosComponent implements OnInit {
+  public todos: Todo[] = [];
 
   constructor() { }
 
