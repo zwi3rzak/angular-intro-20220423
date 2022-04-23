892b53fe14e94b534f3f9ff1e5f705b1eb267752
Adrian Juszczak <adrian@juszczak.io>
2022-04-23 09:55:19 +0200

    Hello Component
    Signed-off-by: Adrian Juszczak <adrian@juszczak.io>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
+import { HelloComponent } from './hello/hello.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HelloComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
