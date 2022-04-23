cf68024bbb8fd3f82e8593079252b6e2bbc5ef59
Adrian Juszczak <adrian@juszczak.io>
2022-04-23 12:02:36 +0200

    Two-way bindings
    Signed-off-by: Adrian Juszczak <adrian@juszczak.io>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import { HelloComponent } from './hello/hello.component';
-import { CounterComponent } from './counter/counter.component';
-import { TodosComponent } from './todos/todos.component';
+import {CounterComponent} from './counter/counter.component';
+import {HelloComponent} from './hello/hello.component';
+import {TodosComponent} from './todos/todos.component';
 
 @NgModule({
   declarations: [AppComponent, HelloComponent, CounterComponent, TodosComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,17 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Todo} from './model/todo';
 
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
-  styleUrls: ['./todos.component.scss']
+  styleUrls: ['./todos.component.scss'],
 })
 export class TodosComponent implements OnInit {
   public todos: Todo[] = [];
+  public text: string = '';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  public clear() {
+    this.text = '';
   }
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
