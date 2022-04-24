28f285adfa4fbd3e1b75ae220d06f47980018cde
Adrian Juszczak <adrian@juszczak.io>
2022-04-24 13:31:27 +0200

    Users
    Signed-off-by: Adrian Juszczak <adrian@juszczak.io>

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Component, OnInit} from '@angular/core';
+import {StorageService} from '../todos/storage.service';
+import {User} from './model/user';
+import {UsersResponse} from './model/users-response';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.scss']
+  styleUrls: ['./users.component.scss'],
 })
 export class UsersComponent implements OnInit {
+  public users: User[] = [];
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    private storage: StorageService,
+  ) {}
 
   ngOnInit(): void {
+    this.getUsers().subscribe(response => {
+      this.users = response.data;
+    });
   }
 
+  public getUsers() {
+    return this.http.get<UsersResponse>(
+      'https://reqres.in/api/users',
+    );
+  }
 }
